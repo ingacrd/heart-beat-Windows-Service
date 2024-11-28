3a4b1c5936b5374e234a26d660c714aefd8e96dc
Carolina Ruiz <ing.acrd@gmail.com>
2024-11-28 11:34:57 -0500

    heart beat windows service console program

---
+++ SimpleHearBeatService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SimpleHearBeatService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x => 
+            { 
+                x.Service<Heartbeat>(s => 
+                {
+                    s.ConstructUsing(heartbeat => new Heartbeat());
+                    s.WhenStarted(hearbeat => hearbeat.Start());
+                    s.WhenStopped(hearbeat => hearbeat.Stop());
+                    s.WhenStopped(hearbeat => hearbeat.Stop());
+
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("HeartbetService");
+                x.SetDisplayName("Heartbeat Service");
+                x.SetDescription("This is the sample heartbeat service used in a demo, just learning Windows Services");
+
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
